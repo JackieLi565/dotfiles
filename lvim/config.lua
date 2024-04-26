544ca96aa23890c1762796ad7d5d4dd4638a08c1
JackieLi565 <li.jackie565@gmail.com>
2024-04-26 15:16:15 -0400

    init

---
+++ lvim/config.lua
@@ -0,0 +1,20 @@
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "java",
+  "yaml",
+  "html",
+  "go",
+  "markdown",
+}
+
+vim.opt.wrap = true
+vim.opt.textwidth = 80
+
